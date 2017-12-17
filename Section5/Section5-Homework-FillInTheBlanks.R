96079c6fd88d48c4a7985fedf9fee6f082e6a6be
Chase <tiongyaocong@gmail.com>
2017-12-17 21:55:02 +0800

    section 5 hmk and dataset

--- Section5/Section5-Homework-FillInTheBlanks.R
+++ Section5/Section5-Homework-FillInTheBlanks.R
@@ -1,67 +1,71 @@
 #Set the Working Directory
 getwd()
-setwd("_")
+setwd("/Users/Chase/R-PROGRAMMING-A-Z/Section5")
 getwd()
 
 #Import the csv dataset
-data <- _("Section5-Homework-Data.csv")
+data <- read.csv("Section5-Homework-Data.csv")
 
 #Explore the data
 data
-_(data)      #check top 6 rows
-_(data, _=7) #check bottom 7 rows
-_(data)      #check the structure of the data frame
-_(data)      #check the summary of the data
+head(data)      #check top 6 rows
+tail(data, n=7) #check bottom 7 rows
+str(data)      #check the structure of the data frame
+summary(data)      #check the summary of the data
 
 #Did you pick up that there is more than one year in the data?
 #From the challenge we know that there are two: 1960 and 2013
 
 #Filter the dataframes
-data1960 <- data[data$_==1960,]
-data2013 <- __________
+data1960 <- data[data$Year==1960,]
+data2013 <- data[data$Year==2013,]
 
 #Check row counts
-_(data1960) #187 rows
-_(data2013) #187 rows. Equal split.
+nrow(data1960) #187 rows
+nrow(data2013) #187 rows. Equal split.
 
 #Create the additional dataframes
-add1960 <- _(Code=_, Life.Exp=_)
-add2013 <- _(_=_, _=_)
+add1960 <- data.frame(Code=Country_Code, Life.Exp=Life_Expectancy_At_Birth_1960)
+add2013 <- data.frame(Code=Country_Code, Life.Exp=Life_Expectancy_At_Birth_2013)
 
 #Check summaries
-_(add1960)
-_(add2013)
+summary(add1960)
+summary(add2013)
 
 #Merge the pairs of dataframes  
-merged1960 <- _(data1960, add1960, _="Country.Code", _="Code")
-merged2013 <- _(_, _, _=_, _=_)
+merged1960 <- merge(data1960, add1960, by.x="Country.Code", by.y="Code")
+merged2013 <- merge(data2013, add2013, by.x="Country.Code", by.y="Code")
 
 #Check the new structures
-_(merged1960)
-_(merged2013)
+str(merged1960)
+str(merged2013)
 
 #We can see an obsolete column in each of the merged dataframes
 #Column "Year" is no longer required. Let's remove it
-merged1960$_ <- _
-___ <- _
+merged1960$Year <- NULL
+merged2013$Year <- NULL
 
 #Check structures again
-_(merged1960)
-_(merged2013)
+str(merged1960)
+str(merged2013)
 
 #Visualization time
-library(_)
+library(ggplot2)
+
+myplot <- function(data,title) {
+  qplot(data=data, x=Fertility.Rate, y=Life.Exp,
+        colour=Region,                               #colour
+        size=I(5), 
+        alpha=I(0.6),                               #transparency
+        main=title #title
+  )
+}
 
 #Visualize the 1960 dataset
-_(_=merged1960, _=Fertility.Rate, _=Life.Exp,
-      _=Region,                               #colour
-      size=_(5), 
-      _=_(0.6),                               #transparency
-      _="Life Expectancy vs Fertility (1960)" #title
-   )
+myplot(merged1960, "Life Expectancy vs Fertility (1960)")
 
 #Visualize the 2013 dataset
-_
+myplot(merged2013, "Life Expectancy vs Fertility (2013)")
 
 
 
